449f53a0424413972a087fdd251d9a1ca4edebee
MathildeBettembourg <mathilde.bettembourg@gmail.com>
2022-10-13 09:51:07 +0200

    service

--- my-app/src/Pages/PageAffichageLocataires.jsx
+++ my-app/src/Pages/PageAffichageLocataires.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { service } from "../Service/Service";
 
 export const PageAffichageLocataire=()=>{
+
+    const[locatairesList, setLocatairesList]=useState([])
+
+    const getLocataires=()=>{
+        service.seeLocataires().then((res)=> setLocatairesList(res))
+    }
+
+    useEffect(()=>{
+        getLocataires()
+    }, 
+    [])
 
 
     return(
