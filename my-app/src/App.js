4963269ce0478c8002a8d33a0968d1b5a18449d7
MathildeBettembourg <mathilde.bettembourg@gmail.com>
2022-10-13 09:28:37 +0200

    creation nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import PageAffichageLocataire from './Pages/PageAffichageLocataires';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand Link to="/PageAffichageLocataire">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link><Link to="/PageAffichageLocataire">Locataires</Link></Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<PageAffichageLocataire/>}/>
+        <Route path='/PageAffichageLocataire' element={<PageAffichageLocataire/>}/>
+      </Routes>
+      </Router>
+    </>
+
   );
 }
 
---
+++ my-app/src/Pages/PageAffichageLocataires.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const PageAffichageLocataire=()=>{
+
+
+    return(
+        <p> test</p>
+    )
+}
+export default PageAffichageLocataire;
